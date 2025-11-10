06ae17ebf861a417f50c013c7dba8fafe2b31a79
PaluszkiewiczS <99697981+PaluszkiewiczS@users.noreply.github.com>
2025-11-10 12:42:12 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     let backgroundCanvas = document.createElement("div");
     backgroundCanvas.style.cssText = "position: fixed; top: 50%; left: 50%; width: 0; height: 0; z-index: 1; \
-    background: rgba(0,0,0,0.9) none center/contain no-repeat; transition: all .8s ease-out; cursor: zoom-out;";
+    background: rgba(0,0,0,0.3) none center/contain no-repeat; transition: all .5s ease-out; cursor: zoom-out;";
     document.body.appendChild(backgroundCanvas);
     backgroundCanvas.onclick = function() { 
         backgroundCanvas.style.inset = "50% auto auto 50%";
